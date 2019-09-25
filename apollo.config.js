74f022958d14aefed73c9b8135824cd7126cbd4b
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2019-09-25 14:53:43 -0700

    ?

--- apollo.config.js
+++ apollo.config.js
@@ -1,13 +1,9 @@
-const fs = require('fs');
 module.exports = {
   client: {
     includes: ["./graphql/*.gql"],
     service: {
       name: 'github',
-      url: 'https://api.github.com/graphql',
-      headers: {
-        authorization: 'Bearer ' + fs.readFileSync('../api-auth-token.txt', { encoding: "utf-8" })
-      },
+      localSchemaFile: './github.json'
     }
   }
 };
